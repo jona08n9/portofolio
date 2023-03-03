1b3d8e78832b18038abdf1b5ce192d3925ffb429
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-03-03 09:41:07 +0100

    A lot of changes. Menu nice, home nice. need litle adjustmens to margins on desktop

--- app.js
+++ app.js
@@ -9,10 +9,25 @@ function loadPage() {
 // * * * * * EVENT LISTENERS
 
 function addPageListeners() {
-  document.querySelector("#menuIcon").addEventListener("click", openMobileMenu);
+  window.addEventListener("resize", homeAnimation);
+  homeAnimation();
 }
 
 // * * * * * FUNCTIONS
+
+function homeAnimation() {
+  if (window.innerWidth < 500) {
+    document.querySelector("#menuIcon").addEventListener("click", openMobileMenu);
+    document.querySelector("#sidebar").removeEventListener("click", openDesktopMenu);
+    document.querySelector("#home").classList.add("mobile");
+    document.querySelector(".h1HomeBr").classList.remove("hidden");
+  } else {
+    document.querySelector("#sidebar").addEventListener("mouseover", openDesktopMenu);
+    document.querySelector("#menuIcon").removeEventListener("click", openMobileMenu);
+    document.querySelector("#home").classList.remove("mobile");
+    document.querySelector(".h1HomeBr").classList.add("hidden");
+  }
+}
 
 function openMobileMenu() {
   document.querySelector("#menuIcon").removeEventListener("click", openMobileMenu);
@@ -28,4 +43,20 @@ function closeMobileMenu() {
   document.querySelector("#menuIcon").classList.add("hideSideBar");
   document.querySelector("#sidebar").classList.add("hideSideBar");
   document.querySelector("#menuIcon").addEventListener("click", openMobileMenu);
+}
+
+function openDesktopMenu() {
+  document.querySelector("#sidebar").removeEventListener("mouseover", openDesktopMenu);
+  document.querySelector("#sidebar").classList.remove("hideSideBar");
+  document.querySelector("#menuIcon").classList.remove("hideSideBar");
+  document.querySelector("#menuIcon").classList.add("showSideBar");
+  document.querySelector("#sidebar").addEventListener("mouseout", closeDesktopMenu);
+}
+
+function closeDesktopMenu() {
+  document.querySelector("#sidebar").removeEventListener("mouseout", closeDesktopMenu);
+  document.querySelector("#menuIcon").classList.remove("showSideBar");
+  document.querySelector("#menuIcon").classList.add("hideSideBar");
+  document.querySelector("#sidebar").classList.add("hideSideBar");
+  document.querySelector("#sidebar").addEventListener("mouseover", openDesktopMenu);
 }
