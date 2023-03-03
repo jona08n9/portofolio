6f630eb4785b0bcd6275b2e455f5929d123cad09
jona08n9 <99641671+jona08n9@users.noreply.github.com>
2023-03-03 11:02:55 +0100

    changes to menu bar responsivity

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ function loadPage() {
 
 function addPageListeners() {
   window.addEventListener("resize", homeAnimation);
+  let links = document.querySelectorAll(".navLink");
+  links.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+    console.log("Added LISTENERS to links. Kh Jonas");
+  });
   homeAnimation();
 }
 
@@ -17,15 +22,41 @@ function addPageListeners() {
 
 function homeAnimation() {
   if (window.innerWidth < 500) {
+    console.log("HomeAnimation: Window<500");
     document.querySelector("#menuIcon").addEventListener("click", openMobileMenu);
-    document.querySelector("#sidebar").removeEventListener("click", openDesktopMenu);
+    document.querySelector("#sidebar").removeEventListener("mouseover", openDesktopMenu);
     document.querySelector("#home").classList.add("mobile");
     document.querySelector(".h1HomeBr").classList.remove("hidden");
   } else {
+    console.log("HomeAnimation: Window<500");
     document.querySelector("#sidebar").addEventListener("mouseover", openDesktopMenu);
     document.querySelector("#menuIcon").removeEventListener("click", openMobileMenu);
     document.querySelector("#home").classList.remove("mobile");
     document.querySelector(".h1HomeBr").classList.add("hidden");
+  }
+}
+
+function closeMenu() {
+  if (window.innerWidth < 500) {
+    console.log("Window<500");
+    document.querySelector("#menuIcon").removeEventListener("click", closeMobileMenu);
+    document.querySelector("#menuIcon").classList.remove("showSideBar");
+    document.querySelector("#menuIcon").classList.add("hideSideBar");
+    document.querySelector("#sidebar").classList.add("hideSideBar");
+    document.querySelector("#menuIcon").addEventListener("click", openMobileMenu);
+  } else {
+    console.log("Window>500");
+    document.querySelector("#sidebar").removeEventListener("mouseout", closeDesktopMenu);
+    document.querySelector("#sidebar").removeEventListener("mouseover", openDesktopMenu);
+    document.querySelector("#sidebar").classList.add("hideSideBar");
+    document.querySelector("#sidebar").classList.add("hideSideBar");
+    document.querySelector("#sidebar").addEventListener("mouseover", waitForClose);
+  }
+
+  function waitForClose() {
+    document.querySelector("#sidebar").removeEventListener("mouseover", waitForClose);
+    console.log("TimeOutBruh");
+    setTimeout(homeAnimation, 1300);
   }
 }
 
@@ -38,6 +69,7 @@ function openMobileMenu() {
 }
 
 function closeMobileMenu() {
+  console.log("eyy");
   document.querySelector("#menuIcon").removeEventListener("click", closeMobileMenu);
   document.querySelector("#menuIcon").classList.remove("showSideBar");
   document.querySelector("#menuIcon").classList.add("hideSideBar");
@@ -48,15 +80,11 @@ function closeMobileMenu() {
 function openDesktopMenu() {
   document.querySelector("#sidebar").removeEventListener("mouseover", openDesktopMenu);
   document.querySelector("#sidebar").classList.remove("hideSideBar");
-  document.querySelector("#menuIcon").classList.remove("hideSideBar");
-  document.querySelector("#menuIcon").classList.add("showSideBar");
   document.querySelector("#sidebar").addEventListener("mouseout", closeDesktopMenu);
 }
 
 function closeDesktopMenu() {
   document.querySelector("#sidebar").removeEventListener("mouseout", closeDesktopMenu);
-  document.querySelector("#menuIcon").classList.remove("showSideBar");
-  document.querySelector("#menuIcon").classList.add("hideSideBar");
   document.querySelector("#sidebar").classList.add("hideSideBar");
   document.querySelector("#sidebar").addEventListener("mouseover", openDesktopMenu);
 }
